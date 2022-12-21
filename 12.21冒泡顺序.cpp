0e57747de41d6cf877c370afbdcd3c9f37d7fa94
BalugaLand <114586562+BalugaLand@users.noreply.github.com>
2022-12-21 13:29:35 +0800

    Create 12.21冒泡顺序.cpp

---
+++ 12.21冒泡顺序.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n,i,j,temp,h[101];
+	cin>>n;
+	for(i=1; i<=n; i++) {
+		cin>>h[i];
+	}
+	for(i=1; i<n; i++) {
+		for (j=1; j<=n-i; j++) {
+
+			if(h[j]>h[j+1]) {
+				temp = h[j];
+				h[j] = h[j+1];
+				h[j+1]=temp;
+			}
+		}
+	}
+	for( i= 1; i < n ; i++) {
+		cout<<h[i]<<" ";
+	}
+	cout<<h[n]<<endl;
+	return 0;
+}
